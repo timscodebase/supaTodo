31de74a68003fcaa25b58f190fe02dadc16255d0
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-02-13 19:11:29 -0800

    first commit

---
+++ src/lib/index.ts
@@ -0,0 +1,20 @@
+import Alert from './components/Alert.svelte'
+export { Alert }
+
+import Auth from './components/Auth.svelte'
+export { Auth }
+
+import Home from './components/Home.svelte'
+export { Home }
+
+import Todo from './components/Todo.svelte'
+export { Todo }
+
+import TodoList from './components/TodoList.svelte'
+export { TodoList }
+
+import { supabase } from './db'
+export { supabase }
+
+import type { Json } from './schema'
+export { Json }
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import './app.css'
+</script>
+
+<svelte:head>
+	<title>Supabase Todo List Example</title>
+</svelte:head>
+
+<slot />
+
+<style>
+</style>
